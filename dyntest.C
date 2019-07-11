3b8bb65a8f5e5159a7cc6c76f408df2716ebf066
Ben Woodard <woodard@redhat.com>
2019-07-11 07:28:20 -0700

    Added local variables Drill down to types

--- dyntest.C
+++ dyntest.C
@@ -17,6 +17,8 @@ using namespace SymtabAPI;
 static const unsigned CACHELINE_BITS=6;
 static const unsigned CLINE_SIZE=100;
 
+static void print_type( localVar *lvar);
+
 int main(int argc, char **argv){
   //Name the object file to be parsed:
   std::string file;
@@ -67,6 +69,7 @@ int main(int argc, char **argv){
     lastone=i;
   }
 
+  
   auto print_vars = [](const auto& p){
 		      std::cout << p->getModule()->fullName() << ": " << std::hex
 				<< p->getOffset() << std::dec 
@@ -83,17 +86,66 @@ int main(int argc, char **argv){
 		    std::for_each(v.begin(),v.end(), print_vars);
 		  }
 		});
-		
+
+  // It looks like BSS variables are present but they are of unknown type.
+  // https://github.com/dyninst/dyninst/issues/619
   
+  // std::vector <Symbol *> syms;
+  // if (obj->findSymbol(syms, "__bss_start", Symbol::ST_UNKNOWN)) {
+  //   std::cout << std::endl << syms[0]->getType() << std::endl;
+  // } else
+  //   std::cout << "elsewhere" << std::endl;
+  // but all the BSS variables don't appear in the list of variable.s
+  // vars.clear();
+  // if (obj->findVariablesByName(vars, "__bss_start")) {
+  //   std::cout << std::endl << vars[0] << std::endl;
+  // }
+
   //  std::for_each(vars.begin(),vars.end(),print_vars);
   // std::cout << vars[0]->getType() << ' ' << vars[0]->getOffset() << ' ' << *vars[0] << std::endl;
   
-  // std::vector <Function *> funcs;
+  std::vector <Function *> funcs;
   // if (!obj->getAllFunctions(funcs))
   //   exit(2);
   // auto print_funcs = [](const auto& p){
-  // 		 std::cout << p->getName() << std::endl; };
+  // 		       std::cout << *(p->typed_names_begin()) << '\t'
+  // 				 << p->getName() << std::endl; };
   // std::for_each(funcs.begin(),funcs.end(),print_funcs);
 
+  funcs.clear();
+  if (!obj->findFunctionsByName(funcs,
+				"_ZN8rajaperf9polybench19POLYBENCH_JACOBI_2D9runKernelENS_9VariantIDE"))
+    exit(4);
+  if(funcs.size()!=1)
+    exit(5);
+  std::vector <localVar *> lvars;
+  (*funcs.begin())->getParams(lvars);
+  //  std::cout << params.size() << std::endl;
+  (*funcs.begin())->getLocalVariables(lvars);
+  std::cout << lvars.size() << std::endl;
+  std::for_each(lvars.begin(),lvars.end(),print_type);
   exit(0);
+}
+
+void print_type( localVar *lvar){
+  Type *t=lvar->getType();
+  std::cout << lvar->getName() << '\t' << t->getName() << ' ';
+  if(t->getDataClass()==dataTypedef)
+    do{
+      std::cout << "\n\ttypedef of "
+		<< t->getTypedefType()->getConstituentType()->getName() << ' ';
+      t=t->getTypedefType()->getConstituentType();
+    }while(t->getDataClass()==dataTypedef);
+  else
+    std::cout << t->getDataClass();
+  std::cout << ' ' << t->getSize() << std::endl;
+
+  std::vector<VariableLocation> &ll=lvar->getLocationLists();
+  std::cout << "\tLocation Lists" << std::endl;
+  std::for_each(ll.begin(),ll.end(),
+		[](const auto &p){
+		  std::cout << "\t\t" << p.stClass << ' ' << p.lowPC
+			    << ' ' << p.hiPC << std::endl;});
+  std::cout << std::endl;
+				      
 }
